e51ccc6760dadda29365199880b0a8e16c38abe8
Artest <kav3109@gmail.com>
2020-08-07 21:14:03 +0300

    hw9(50%)

--- src/app/core/data/personals.mock.ts
+++ src/app/core/data/personals.mock.ts
@@ -4,7 +4,7 @@ export const PERSONALS: Array<Personal> = [
   {
     photo: 'https://www.gstatic.com/tv/thumb/persons/28555/28555_v9_ba.jpg',
     name: 'Jackie Chan',
-    phone: [381112223344, 123412341234],
+    phone: [381112223344],
     city: 'HongKong',
     incomes: 1101656695,
     segments: [{
